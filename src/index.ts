5b04037724966ea37c95031324fbedc0d4531a34
Luis Ramirez <lou81804@gmail.com>
2022-06-19 12:43:49 -0500

    Setting User model and event handler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello world!')
+import { User } from "./models/User"
+
+const user = new User({name: 'Luis', age:20})
+
+user.on('click',() => console.log('Clicking on'))
+user.trigger('click')
---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+interface UserProps {
+    name?: string
+    age?: number
+}
+
+type Callback = () => void
+
+export class User {
+
+    private events: { [key: string]: Callback[] }
+
+    constructor(private data: UserProps) {
+        this.events = {}
+    }
+
+    get(propName: string): string | number {
+        return this.data[propName]
+    }
+
+    set(update: UserProps): void {
+        Object.assign(this.data,update)
+    }
+
+    on(eventName: string, callback: Callback): void{
+        const handler = this.events[eventName] || []
+        handler.push(callback)
+        this.events[eventName] = handler
+    }
+
+    trigger(eventName: string): void {
+        const handler = this.events[eventName]
+        if (!handler || handler.length === 0 ) return
+
+        handler.forEach(callback => callback())
+    }
+}
